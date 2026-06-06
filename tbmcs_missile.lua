b2a07f93f8eb51820d38e5d1fe5c9f2e284eb27b
nexus331 <memelordyt53@gmail.com>
2026-06-06 11:57:47 +0800

    Added Basic Guidance System and Payload Delivery

--- tbmcs_missile.lua
+++ tbmcs_missile.lua
@@ -4,16 +4,39 @@
 -- Software Initialization
 modem_location = peripheral.find("modem", rednet.open)
 
-
+redstone.setOutput("back", true)
 function wait_command()
     print("Waiting command....")
     while true do
         id, message, protocol = rednet.receive(tbmcs)
         print("DEBUG:", message)
         if message == "MISSILE" then
+        -- FLIGHT GUIDANCE
+        
             redstone.setOutput("bottom", true)
-            sleep(0.4)
+            sleep(4)
+            redstone.setOutput("right", true)
+            sleep(0.8)
+            redstone.setOutput("right", false)
+            sleep(3)
+            redstone.setOutput("bottom", true)
+            redstone.setOutput("left", true)
+            sleep(2)
+            redstone.setOutput("left", false)
+            redstone.setOutput("bottom", true)
             redstone.setOutput("bottom", false)
+            sleep(0.5)
+            
+            -- WARHEAD 
+            redstone.setOutput("back", false)
+            print("WARHEAD DETACHED!")
+            sleep(0.3)
+            redstone.setOutput("left", true)
+            redstone.setOutput("bottom",true)
+            redstone.setOutput("back", true)
+            sleep(4)
+            redstone.setOutput("bottom", false)
+            redstone.setOutput("left",false)
         end
     end
 end
